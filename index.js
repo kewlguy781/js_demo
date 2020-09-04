77a74506ea7b8ef148ebf2fc7306b2c93d95f054
Jon Roberts <kewlguy@gmail.com>
2020-09-03 20:29:18 -0600

    Added todo and clean up

--- index.js
+++ index.js
@@ -7,14 +7,40 @@
 //     {title: "Super Mario Odsessy", platform:"Nintendo Switch"},
 // ];
 
+//Main Header
 let state = {
     pageHeader: "Video Games List",
     games: games,
   };
 
+  // List games
+  
+  // Rendering all information into "cache" gamestring, then RETURN that will be printed
+  function renderGames() {
+    gamesString = "";
+    state.games.forEach(function (games, index) {
+      gamesString += `<div>
+      Game: ${games.title} Platform: ${games.platform} [Edit Games][Delete Game]
+      </div>`;
+    });
+    //THIS IS A MUST! Return this which will then be printed when called
+    return gamesString;
+  }
+
+  // Add a game to the list
+
+  // Edit a games to the list
+
+  // Delete a game from the list
+
+
+
+
+  //Rendering the main part
   function render() {
     htmlString = `<div>
                   <h1>${state.pageHeader}</h1>
+                  ${renderGames()} [Add Games]
                   </div>`;
 
     document.getElementById("app").innerHTML = htmlString;
